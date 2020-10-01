28d26f534cd3f5661c35afc00331c2901690794d
Shady Coder <shadysaleh01@gmail.com>
2020-09-30 17:46:24 -0700

    Function to display all the columns of any table passed by

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,16 @@
+const connection = require("./connection")
 const orm = require("/connection.js")
+
+
+var orm = {
+   selecAll: function (tableName, cb) {
+      const query = "SELECT * FROM" + tableName + ";"
+      connection.query(query, (err, result) => {
+         if (err) throw err
+         cb(result)
+      })
+   },
+
+}
+
+module.exports = orm
