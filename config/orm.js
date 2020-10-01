ca9be75e585f2604d90067f2d8888bb9c7c59e3e
Shady Coder <shadysaleh01@gmail.com>
2020-09-30 23:32:34 -0700

    Function to display all burger table - inside burger.js file

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,15 @@
 const connection = require("./connection")
-const orm = require("/connection.js")
-
 
 var orm = {
-   selecAll: function (tableName, cb) {
-      const query = "SELECT * FROM" + tableName + ";"
+   selectAll: function (tableName, cb) {
+      const query = `SELECT * FROM ${tableName};`
       connection.query(query, (err, result) => {
          if (err) throw err
          cb(result)
       })
    },
-   insertOne: function (burgers_name, devoured, cb) {
-      const query = "INSERT INTO burgers (burgers_name, devoured) VALUES " + (burgers_name, devoured)
+   insertOne: function (tableName, column, value, cb) {
+      const query = `INSERT INTO ${tableName} (${column}) VALUES (${value})`
       connection.query(query, (err, result) => {
          if (err) throw err
          cb(result)
--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,13 @@
 const orm = require("../config/orm")
 
+
+const burger = {
+   selectAll: function (cb) {
+      orm.selectAll("burger", function (res) {
+         cb(res)
+      })
+   },
+
+
+
+}
