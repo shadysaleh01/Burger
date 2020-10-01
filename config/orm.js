2a560ceb448c42026c72f7847e2d009b9fe4ae21
Shady Coder <shadysaleh01@gmail.com>
2020-10-01 15:14:58 -0700

    Fixed tha name of the table was missing leter 's'

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,7 @@ const connection = require("./connection.js")
 
 var orm = {
    selectAll: function (tableName, cb) {
-      const query = `SELECT * FROM ${tableName};`
+      const query = `SELECT * FROM ${tableName}`
       connection.query(query, (err, result) => {
          if (err) throw err
          cb(result)
--- models/burger.js
+++ models/burger.js
@@ -3,17 +3,17 @@ const orm = require("../config/orm.js")
 
 const burger = {
    selectAll: function (cb) {
-      orm.selectAll("burger", function (res) {
+      orm.selectAll("burgers", function (res) {
          cb(res)
       })
    },
    insertOne: function (column, value, cb) {
-      orm.insertOne("burger", column, value, function (res) {
+      orm.insertOne("burgers", column, value, function (res) {
          cb(res)
       })
    },
    updateOne: function (id, cb) {
-      orm.updateOne("burger", "devoured", true, id, function (res) {
+      orm.updateOne("burgers", "devoured", true, id, function (res) {
          cb(res)
       })
    }
