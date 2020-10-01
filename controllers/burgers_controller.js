92144d9599e51febd94243198d6e079949537c8d
Shady Coder <shadysaleh01@gmail.com>
2020-10-01 01:02:15 -0700

    Created router to the home page displays the burger table

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,12 @@ const express = require("express")
 const router = express.Router()
 const burger = require("../models/burger")
 
+router.get("/", (req, res) => {
+   burger.selectAll((data) => {
+      let hbsObject = {
+         burger: data
+      }
+      res.render("index", hbsObject)
+   })
+})
+
