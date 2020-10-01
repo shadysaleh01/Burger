be4d2e0a2b8716443b18acce90efacde66fd2cce
Shady Coder <shadysaleh01@gmail.com>
2020-10-01 00:07:59 -0700

    Function to create new row inside burger table - burger.js file

--- models/burger.js
+++ models/burger.js
@@ -7,7 +7,11 @@ const burger = {
          cb(res)
       })
    },
-
+   insertOne: function (column, value, cb) {
+      orm.insertOne("burger", column, value, cb, function (res) {
+         cb(res)
+      })
+   }
 
 
 }
